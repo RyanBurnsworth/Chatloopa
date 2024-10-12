fb54473e470ba6b52e57a5cf8864554933953d89
Ryan Burnsworth <ryanburnsworth@gmail.com>
2024-10-12 19:57:08 -0400

    Revert "upgrade libraries; fix minor bugs in text chat"
    This reverts commit 68d172357689e876dc2b84764c42c6c9a710b630.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'text', component: TextChatComponent },
   { path: 'video', component: WebConferenceComponent },
+  { path: '**', redirectTo: '/' } // Wildcard route for 404 handling
 ];
 
 @NgModule({
